5835f7c76a20ec779a2267ef613a0ff3e904fb3b
Yashas Donthi <32161167+coolyashas@users.noreply.github.com>
2023-11-03 16:27:51 +0530

    Create detect_c.py

---
+++ detect_c.py
@@ -0,0 +1,24 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+
+class NewFileHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory or not event.src_path.endswith(".c"):
+            return
+        new_file = os.path.basename(event.src_path)
+        os.system(f"python3 automate.py {new_file}")
+
+if __name__ == "__main__":
+    path_to_watch = "Path to your C working repository"
+    event_handler = NewFileHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=path_to_watch, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
